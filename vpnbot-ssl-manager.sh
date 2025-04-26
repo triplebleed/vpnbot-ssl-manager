e484c35d6d12c063b5d8949dbb21c52b5a61446c
Maxim Balashov <82332728+triplebleed@users.noreply.github.com>
2025-04-27 00:23:26 +0300

    Update vpnbot-ssl-manager.sh

--- vpnbot-ssl-manager.sh
+++ vpnbot-ssl-manager.sh
@@ -160,10 +160,13 @@ server {
 
     location / {
         proxy_pass http://$service_ip;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "Upgrade";
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto https;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 }
 $end_marker
